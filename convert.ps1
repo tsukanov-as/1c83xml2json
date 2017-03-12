549bcc5696b76644c08a37fea4df9f8022c8bfae
tsukanov-as <tsukanov.as@yandex.ru>
2017-03-12 19:31:33 +0300

    Update convert.ps1

--- convert.ps1
+++ convert.ps1
@@ -1,24 +1,56 @@
 $path = 'C:\temp\Config'
 
+#----------------------------------------------------------------------------------------------------------------------
+
 $list = @()
 
-Get-ChildItem "$path\Documents" -Filter *.xml |
-Foreach-Object {
+Get-ChildItem "$path\Documents" -Filter *.xml | % {
+    
     [xml]$Data = Get-Content $_.FullName
+    
     $Prop = $Data.MetaDataObject.Document.Properties
+    
     $Synonyms = $Prop.Synonym.ChildNodes 
     $SynonymEn = $Synonyms.Where{$_.lang -eq 'en'}
     $SynonymRu = $Synonyms.Where{$_.lang -eq 'ru'}
-    $obj = @{}
-    $obj.Name = $Prop.Name
-    $obj.Synonym = @{en=$SynonymEn.Content;ru=$SynonymRu.Content}
-    $obj.Posting = $Prop.Posting
-    $obj.RealTimePosting = $Prop.RealTimePosting
-    $obj.RegisterRecordsDeletion = $Prop.RegisterRecordsDeletion
-    $obj.RegisterRecordsWritingOnPost = $Prop.RegisterRecordsWritingOnPost
-    $list += $obj 
+    
+    $list += $Prop |
+    select `
+        Name,
+        @{Name="Synonym";Expression={@{en=$SynonymEn.Content;ru=$SynonymRu.Content}}},
+        Posting,
+        RealTimePosting,
+        RegisterRecordsDeletion,
+        RegisterRecordsWritingOnPost 
 }
 
 $list | ConvertTo-Json -Depth 2 | Out-File Documents.json -Encoding 'utf8'
 Compress-Archive -Path Documents.json -DestinationPath Documents.zip -Force
 Remove-Item Documents.json
+
+#----------------------------------------------------------------------------------------------------------------------
+
+$list = @()
+
+Get-ChildItem "$path\Catalogs" -Filter *.xml | % {
+    
+    [xml]$Data = Get-Content $_.FullName
+    
+    $Prop = $Data.MetaDataObject.Catalog.Properties
+    
+    $Synonyms = $Prop.Synonym.ChildNodes 
+    $SynonymEn = $Synonyms.Where{$_.lang -eq 'en'}
+    $SynonymRu = $Synonyms.Where{$_.lang -eq 'ru'}
+
+    $list += $Prop |
+    select `
+        Name,
+        @{Name="Synonym";Expression={@{en=$SynonymEn.Content;ru=$SynonymRu.Content}}},
+        Hierarchical,
+        HierarchyType,
+        LimitLevelCount
+}
+
+$list | ConvertTo-Json -Depth 2 | Out-File Catalogs.json -Encoding 'utf8'
+Compress-Archive -Path Catalogs.json -DestinationPath Catalogs.zip -Force
+Remove-Item Catalogs.json
